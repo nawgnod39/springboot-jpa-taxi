4519fc386bf5db4231bcc4e9e03068b7216a6057
dongwantak <dongwantak@gmail.com>
2021-04-09 19:50:04 +0900

    yml 설정

---
+++ src/main/java/com/example/hammertaxi/test/Member.java
@@ -0,0 +1,26 @@
+package com.example.hammertaxi.test;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+public class Member {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+
+	@Builder
+	public Member(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+}
