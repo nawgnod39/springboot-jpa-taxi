6f3f5903ff2cb2e8d181a07c1742c62d4d4b3f4e
dongwantak <dongwantak@gmail.com>
2021-04-09 23:57:43 +0900

    database user,board ,reply create

---
+++ src/main/java/com/example/hammertaxi/model/User.java
@@ -0,0 +1,45 @@
+package com.example.hammertaxi.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴!!
+//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다. 자동으로 생성 
+public class User {
+
+	@Id //Primary key 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다. 
+	private int id; // 시퀀스, auto_increment 
+
+	@Column(nullable = false, length = 30) 
+	private String username; // 아이디
+
+	@Column(nullable = false, length = 100) // 123456 => 해쉬 (비밀번호 암호화)하기위해 길게 씀 .
+	private String password;
+
+	@Column(nullable = false, length = 50)
+	private String email; // myEmail, my_email
+
+	@ColumnDefault("'user'")//""를 넣어야한다.
+	private String role; // Enum을 쓰는게 좋다. // admin, user, manager 권한을 만들어 줌 
+
+	@CreationTimestamp
+	private LocalDateTime createDate;//가입한시각을 나타냄. 
+}
