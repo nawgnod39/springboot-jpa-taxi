0dcf8c2e7348e119fd6249414e7c03d575340607
dongwantak <dongwantak@gmail.com>
2021-04-29 00:45:07 +0900

    detail.js 버튼 값 수정

--- src/main/java/com/example/hammertaxi/controller/api/BoardApiController.java
+++ src/main/java/com/example/hammertaxi/controller/api/BoardApiController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class BoardApiController {
@@ -27,6 +27,7 @@ public class BoardApiController {
 		boardService.글삭제하기(id);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); 
 	}
+	
 	@PutMapping("/api/board/{id}")
 	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board){
 		System.out.println("BoardApiController : update : id : "+id);
